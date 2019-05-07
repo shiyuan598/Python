4d3a7b8b9e8290d21b76198820916fd8d64b2c05
wangshiyuan <wangshiyuan@navinfo.com>
2019-05-07 10:55:38 +0800

    wx read auto

---
+++ Basic/WXReadAuto/ADB/wx_autoread_rapid-1.1.py
@@ -0,0 +1,41 @@
+from uiautomator import device as d
+import time
+import datetime
+import random
+
+# 点亮屏幕
+def lightScreen():
+    d.screen.on()
+
+# 自动翻页，翻页后休息几秒钟
+def autoSwipe():
+    # 模拟阅读12-15秒钟
+    total_time = 15
+    read_time = random.randint(12, 15)
+    time.sleep(read_time)
+    print("阅读花费：", read_time, "秒")
+    # 从（1000,500）到（30,500）
+    d.swipe(1000, 500, 30, 500)  # 这里需要根据你的模拟器的具体坐标测试
+    # 休息一段时间
+    rest_time = total_time - read_time
+    time.sleep(rest_time)
+    print("休息" + str(rest_time) + "秒, 放松下眼睛呦~~~")
+
+# 默认阅读300页，大约300*(15)/60 = 75分钟
+if __name__ == '__main__':
+    all_time = 300
+    user_input_time = input("请输入需要阅读的页数(请输入正整数):")
+    try:
+        user_input_time = int(user_input_time)
+        if (user_input_time > 0):
+            print("您本次将阅读", user_input_time, "页，加油！")
+            all_time = user_input_time
+    except:
+        print("您输入的值不合法， 将使用默认参数, 自动阅读300页")
+        pass
+
+    for i in range(all_time * 4):
+        lightScreen()
+        print("开始阅读...")
+        autoSwipe()
+        print("==>已经阅读", i + 1, "页", "还有", all_time - i - 1, "页呦")
---
+++ Basic/WXReadAuto/ADB/wx_read_auto_rapid.py
@@ -0,0 +1,39 @@
+from uiautomator import device as d
+import time
+import datetime
+import random
+
+# 点亮屏幕
+def lightScreen():
+    d.screen.on()
+
+# 自动翻页，翻页后休息2秒钟
+def autoSwipe():
+    # 假装看书5-10秒钟(假装是人类在看书。。。)
+    read_time = random.randint(5, 10)
+    time.sleep(read_time)
+    print("阅读花费：", read_time, "秒")
+    # 从（1000,500）到（30,500）
+    d.swipe(1000, 500, 30, 500)  # 这里需要根据你的模拟器的具体坐标测试
+    # 休息一段时间(休息的时间=60秒-看书的秒数)
+    time.sleep(2)
+    print("休息2秒,放松下眼睛~")
+
+# 默认执行5小时(300分钟)
+if __name__ == '__main__':
+    all_time = 300
+    user_input_time = input("请输入需要阅读的分钟数(请输入正整数):")
+    try:
+        user_input_time = int(user_input_time)
+        if (user_input_time > 0):
+            print("程序将会执行", user_input_time, "分钟")
+            all_time = user_input_time
+    except:
+        print("您输入的值不合法， 将使用默认参数300， 程序将会自动执行5小时")
+        pass
+
+    for i in range(all_time):
+        lightScreen()
+        print("自动点亮屏幕, 开始阅读。。。")
+        autoSwipe()
+        print("==>已经阅读", i + 1, "分钟", "还差", all_time - i - 1, "分钟完成阅读")
